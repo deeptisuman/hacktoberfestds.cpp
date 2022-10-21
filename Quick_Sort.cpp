0da33ee78aef83c00433ce897eb2d604db3e4428
Soham Chowdhury <chowdhurysoham007@gmail.com>
2022-10-21 23:31:31 +0530

    Update Quick_Sort.cpp

--- Quick_Sort.cpp
+++ Quick_Sort.cpp
@@ -1,41 +1,71 @@
-#include<bits/stdc++.h>
+// C++ Implementation of the Quick Sort Algorithm.
+#include <iostream>
 using namespace std;
-void swap(int &a,int &b)
+
+int partition(int arr[], int start, int end)
 {
-    int tem=b;
-    b=a;
-    a=tem;
+
+	int pivot = arr[start];
+
+	int count = 0;
+	for (int i = start + 1; i <= end; i++) {
+		if (arr[i] <= pivot)
+			count++;
+	}
+
+	// Giving pivot element its correct position
+	int pivotIndex = start + count;
+	swap(arr[pivotIndex], arr[start]);
+
+	// Sorting left and right parts of the pivot element
+	int i = start, j = end;
+
+	while (i < pivotIndex && j > pivotIndex) {
+
+		while (arr[i] <= pivot) {
+			i++;
+		}
+
+		while (arr[j] > pivot) {
+			j--;
+		}
+
+		if (i < pivotIndex && j > pivotIndex) {
+			swap(arr[i++], arr[j--]);
+		}
+	}
+
+	return pivotIndex;
 }
-int lomutoPartition(int a[],int l,int h)
+
+void quickSort(int arr[], int start, int end)
 {
-    int pivot=a[h];
-    int i=(l-1);
-    for(int j=l;j<=h-1;j++)
-        if(a[j]<pivot)
-        {
-            i++;
-            swap(a[i],a[j]);
-        }
-    swap(a[i+1],a[h]);
-    return i+1;
+
+	// base case
+	if (start >= end)
+		return;
+
+	// partitioning the array
+	int p = partition(arr, start, end);
+
+	// Sorting the left part
+	quickSort(arr, start, p - 1);
+
+	// Sorting the right part
+	quickSort(arr, p + 1, end);
 }
-void qsort(int a[],int l,int h)
-{
-    if(h>l)
-    {
-        int p=lomutoPartition(a,l,h);
-        qsort(a,l,p-1);
-        qsort(a,p+1,h);
-    }
-}
+
 int main()
 {
-    int n;
-    cin>>n;
-    int a[n];
-    for(int i=0;i<n;i++)
-        cin>>a[i];
-    qsort(a,0,n-1);
-    for(int i=0;i<n;i++)
-        cout<<a[i]<<" ";
+
+	int arr[] = { 9, 3, 4, 2, 1, 8 };
+	int n = 6;
+
+	quickSort(arr, 0, n - 1);
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+
+	return 0;
 }
